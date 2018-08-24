9207df6e573e3cbb8d4eaa15bba5f2c75fe800f3
vitoriopaulo <vitoriopaulo@gmail.com>
2018-08-24 19:12:05 -0400

    File routes.rb updated.
    Step 12 of tutorial done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
-Rails.application.routes.draw do
-  get 'home/index'
+# app/config/routes.rb
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+
+root      'home#index'
+resources :home, only: [:index, :new, :create]
+
 end
